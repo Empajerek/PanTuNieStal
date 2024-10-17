264ea097efa0f8ad72473cd4da0942a8734f4643
Konrad <konradjones@outlook.com>
2024-10-17 11:26:09 +0200

    commented hat verbessern

--- strqueue.cpp
+++ strqueue.cpp
@@ -5,100 +5,91 @@
 #include <string>
 #include <cstddef>
 
-#ifndef DNDEBUG
-    #define DEBUG_FAILED() std::cerr << __func__ << " failed \n";
-    #define DEBUG_DNE(x) std::cerr << __func__ << ": queue" << x << "does not exist \n";
-    #define DEBUG_DNC(x, y) std::cerr << __func__ << ": queue " << x << " does not contain string at position " << y << " \n";
-#endif
+#undef NDEBUG
 
 namespace {
-    void printArg() { }
+#ifndef NDEBUG
+    #define DEBUG_FAILED() std::cerr << __func__ << " failed \n"
+    #define DEBUG_DNE(x) std::cerr << __func__ << ": queue " << x << " does not exist \n"
+    #define DEBUG_DNC(x, y) std::cerr << __func__ << ": queue " << x << " does not contain string at position " << y << " \n"
+    #define DEBUG_START(...) std::cerr << __func__ << "(" << argToStr(__VA_ARGS__) << ") \n"
+    #define DEBUG_RETURN(x) std::cerr << __func__ << returnStr(x) << " \n"
 
-    void printArg(std::nullptr_t _) {
-        std::cerr << "NULL";
+    std::string argToStr() {
+        return "";
     }
 
-    void printArg(const char* arg) {
-        std::cerr << "\"" << arg << "\"";
+    std::string argToStr(const char* arg) {
+        if (arg == nullptr)
+            return "NULL";
+        return  "\"" + std::string(arg) + "\"";
     }
 
     template<typename T>
-    void printArg(const T& arg) {
-        std::cerr << arg;
+    std::string argToStr(const T& arg) {
+        return std::to_string(arg);
     }
 
     template<typename T, typename... Arg>
-    void printArg(const T& arg, const Arg&... rest) {
-        printArg(arg);
-        std::cerr << ", ";
-        printArg(rest...);
+    std::string argToStr(const T& arg, const Arg&... rest) {
+        return argToStr(arg) + ", " + argToStr(rest...);
     }
 
-    template<typename... Args>
-    void printStartDebugInformation(const char* functionName, Args... args) {
-#ifndef DNDEBUG
-        std::cerr << functionName << "(";
-        printArg(args...);
-        std::cerr << ") \n";
-#endif
-    }
-
-    void printReturnDebugInformation(const char* functionName) {
-#ifndef DNDEBUG
-        std::cerr << functionName<< " done \n";
-#endif
-    }
+    std::string returnStr() { return " done"; }
 
     template<typename Arg>
-    void printReturnDebugInformation(const char* functionName, Arg arg) {
-#ifndef DNDEBUG
-        std::cerr << functionName << " returns ";
-        printArg(arg);
-        std::cerr << " \n";
-#endif
+    std::string returnStr(Arg arg) {
+        return " returns " + argToStr(arg);
     }
 
+#else
+    #define DEBUG_FAILED(...) {}
+    #define DEBUG_DNE(...) {}
+    #define DEBUG_DNC(...) {}
+    #define DEBUG_START(...) {}
+    #define DEBUG_RETURN(...) {}
+#endif
+
     std::unordered_map<unsigned long, std::deque<std::string>> queues;
-    unsigned long next_id = 0;
 } // namespace
 
 unsigned long strqueue_new() {
-    printStartDebugInformation(__func__);
-    unsigned long id = next_id++;
+    DEBUG_START();
+    static unsigned long id = 0;
     queues[id] = std::deque<std::string>();
-    printReturnDebugInformation(__func__,id);
-    return id;
+    DEBUG_RETURN(id);
+    return id++;
 }
 
 void strqueue_delete(unsigned long id) {
-    printStartDebugInformation(__func__,id);
+    DEBUG_START(id);
     queues.erase(id);
-    printReturnDebugInformation(__func__);
+    DEBUG_RETURN();
 }
 
 size_t strqueue_size(unsigned long id) {
-    printStartDebugInformation(__func__, id);
+    DEBUG_START(id);
     const auto iter = queues.find(id);
     size_t size;
     if (iter == queues.end()) {
-        DEBUG_DNE(id)
+        DEBUG_DNE(id);
         size = 0;
     } else {
         size = iter->second.size();
     }
-    printReturnDebugInformation(__func__, size);
+    DEBUG_RETURN(size);
     return size;
 }
 
 void strqueue_insert_at(unsigned long id, size_t position, const char* str) {
-    printStartDebugInformation(__func__, id, position, str);
+    DEBUG_START(id, position, str);
     auto iter = queues.find(id);
     if (iter == queues.end()) {
-        DEBUG_DNE(id)
+        DEBUG_DNE(id);
         return;
     }
     if (str == NULL) {
-        DEBUG_FAILED()
+        DEBUG_FAILED();
         return;
     }
 
@@ -106,59 +97,61 @@ void strqueue_insert_at(unsigned long id, size_t position, const char* str) {
         iter->second.push_back(str);
     else
         iter->second.insert(iter->second.begin() + position, str);
-    printReturnDebugInformation(__func__);
+    DEBUG_RETURN();
 }
 
 void strqueue_remove_at(unsigned long id, size_t position) {
-    printStartDebugInformation(__func__, id, position);
+    DEBUG_START(id, position);
     auto iter = queues.find(id);
     if (iter == queues.end()) {
-        DEBUG_DNE(id)
+        DEBUG_DNE(id);
         return;
     }
 
     if (iter->second.size() <= position) {
-        DEBUG_DNC(id, position)
+        DEBUG_DNC(id, position);
         return;
     }
 
     iter->second.erase(iter->second.begin() + position);
-    printReturnDebugInformation(__func__);
+    DEBUG_RETURN();
 }
 
 const char* strqueue_get_at(unsigned long id, size_t position) {
-    printStartDebugInformation(__func__, id, position);
+    DEBUG_START(id, position);
     auto iter = queues.find(id);
     if (iter == queues.end()) {
-        DEBUG_DNE(id)
-        printReturnDebugInformation(__func__, nullptr);
+        DEBUG_DNE(id);
+        DEBUG_RETURN((const char*) 0);
         return NULL;
     }
 
     if (iter->second.size() <= position) {
         DEBUG_DNC(id, position);
-        printReturnDebugInformation(__func__, nullptr);
+        DEBUG_RETURN((const char*) 0);
         return NULL;
     }
 
     const char* elem = iter->second[position].c_str();
-    printReturnDebugInformation(__func__, elem);
+    DEBUG_RETURN(elem);
     return elem;
 }
 
 void strqueue_clear(unsigned long id) {
-    printStartDebugInformation(__func__, id);
+    DEBUG_START(id);
     auto iter = queues.find(id);
     if (iter == queues.end()) {
-        DEBUG_DNE(id)
+        DEBUG_DNE(id);
         return;
     }
 
     iter->second.clear();
-    printReturnDebugInformation(__func__);
+    DEBUG_RETURN();
 }
 
 int strqueue_comp(unsigned long id1, unsigned long id2) {
+    (void) id1;
+    (void) id2;
     return 0;
 }
 
